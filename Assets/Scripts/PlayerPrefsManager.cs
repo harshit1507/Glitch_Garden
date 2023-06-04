35fb0a8cf55d09bb835c69e398f0143288e37b56
Harshit <harshit1507@yahoo.com>
2023-06-04 17:29:05 +0530

    add PlayerPrefsManager class to manage PlayerPrefs

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    private const string MASTER_VOLUME_KEY = "master_volume";
+    private const string DIFFICULTY_KEY = "difficulty";
+    private const string LEVEL_KEY = "level_unlocked_";
+
+    public static void SetMasterVolume(float volume)
+    {
+        if(volume is > 0f and < 1f)
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        else
+            Debug.LogError("Master Volume Out of Range.");
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+    public static void UnlockLevel(int level)
+    {
+        if (level <= SceneManager.sceneCountInBuildSettings - 1)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 1);    //use 1 for true
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level not in build order.");
+        }
+    }
+
+    public static bool IsLevelUnlocked(int level)
+    {
+        int levelValue = PlayerPrefs.GetInt(LEVEL_KEY + level.ToString(), 0);
+        bool isLevelUnlocked = (levelValue == 1)? true : false;
+        
+        if (level <= SceneManager.sceneCountInBuildSettings - 1)
+        {
+            return isLevelUnlocked;
+        }
+        else
+        {
+            Debug.LogError("Trying to query level not in build order.");
+            return false;
+        }
+    }
+
+    public static void SetDifficulty(float difficulty)
+    {
+        if (difficulty is >= 0f and <= 1f)
+        {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
+        }
+        else
+        {
+            Debug.LogError("Difficulty is out of range.");
+        }
+    }
+
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+    }
+}
+
