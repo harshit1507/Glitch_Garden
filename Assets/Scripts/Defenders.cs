8a002df51aee8446c22e7c00cacb5c467695dd33
Harshit <harshit1507@yahoo.com>
2023-06-08 23:32:33 +0530

    add colliders

--- Assets/Scripts/Defenders.cs
+++ Assets/Scripts/Defenders.cs
@@ -1,24 +1,21 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attacker : MonoBehaviour
+public class Defenders : MonoBehaviour
 {
-    [Range(-1f, 1.5f)] [SerializeField] float walkSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody2D rigidbody2D = gameObject.AddComponent<Rigidbody2D>();
-        rigidbody2D.isKinematic = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+        
     }
-
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log(other.name + " trigger enter.");
