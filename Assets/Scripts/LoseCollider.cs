19f2274b67d36b5a791e47ca20e9ca71b21e82cc
Harshit <harshit1507@yahoo.com>
2023-06-17 20:11:03 +0530

    implement lose condition

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 
 public class LoseCollider : MonoBehaviour
 {
+    private LevelManager levelManager;
+
+    private void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        throw new NotImplementedException();
+        levelManager.LoadLevel("Lose Scene");
     }
 }
