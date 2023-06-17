a7ca73051defd6fc493ef25b5fcd8a137de81e56
Harshit <harshit1507@yahoo.com>
2023-06-17 18:37:44 +0530

    defender only shoot if any attacker present in lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -2,21 +2,60 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Shooter : MonoBehaviour
 {
     [SerializeField] private GameObject projectile;
     [SerializeField] private GameObject projectileParent;
     [SerializeField] private GameObject gun;
+    [SerializeField] private Animator anim;
+    [SerializeField] private AttackerSpawner myLaneAttackerSpawner;
 
     private void Start()
     {
-        projectileParent = GameObject.Find("Projectiles");
+        CheckAndCreateParent();
+        anim = GetComponent<Animator>();
+        SetMyLaneAttackerSpawner();
+    }
 
+    private void CheckAndCreateParent()
+    {
+        projectileParent = GameObject.Find("Projectiles");
         if (!projectileParent)
         {
             projectileParent = new GameObject("Projectiles");
         }
+    }
+
+    private void Update()
+    {
+        if (IsAttackerAheadInLane())
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+    }
+
+    private bool IsAttackerAheadInLane()
+    {
+        if (myLaneAttackerSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+
+        foreach (Transform attacker in myLaneAttackerSpawner.transform)
+        {
+            if (attacker.transform.position.x > transform.position.x)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     void Fire()
@@ -29,5 +68,21 @@ public class Shooter : MonoBehaviour
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
+    }
+
+    void SetMyLaneAttackerSpawner()
+    {
+        AttackerSpawner[] attackerSpawners = GameObject.FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner attackerSpawner in attackerSpawners)
+        {
+            if (attackerSpawner.transform.position.y == transform.position.y)
+            {
+                myLaneAttackerSpawner = attackerSpawner;
+                return;
+            }
+        }
+        
+        Debug.LogError(name + " can't find spawner in lane");
     }
 }
